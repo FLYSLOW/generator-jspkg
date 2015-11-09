ee346816b30cedc194696204d914695b6c9ec3b0
FLYSLOW <xufayflying@gmail.com>
2015-11-09 22:29:02 +0800

    Add generators for .editorconfig .eslintrc .gitignore .jshintrc

---
+++ generators/editorconfig/index.js
@@ -0,0 +1,32 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+
+module.exports = yeoman.generators.Base.extend({
+
+  initializing: function () {},
+
+  prompting: function() {
+    var done = this.async();
+    var prompts = [];
+
+    this.prompt(prompts, function(answers) {
+      this.props = answers;
+      done();
+    }.bind(this));
+  },
+
+  configuring: function() {},
+
+  default: {},
+
+  writing: function () {
+    this.template('_editorconfig', '.editorconfig', this.props);
+  },
+
+  conflicts: function() {},
+
+  install: function() {},
+
+  end: function() {}
+
+});
--- test/gitignore.js
+++ test/gitignore.js
@@ -4,9 +4,9 @@ var path = require('path');
 var assert = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
 
-describe('Jspkg:generators/package.json', function () {
+describe('Jspkg:generators/gitignore', function () {
   before(function (done) {
-    helpers.run(path.join(__dirname, '../generators/package.json'))
+    helpers.run(path.join(__dirname, '../generators/gitignore'))
       .withArguments('name')
       .withOptions({ skipInstall: true, force: true })
       .on('end', done);
